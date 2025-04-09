b4de5672507a263e216b4eec8f035dc6764ddf9b
unknown <dadasik89@gmail.com>
2025-04-09 03:23:38 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ЯМайки - Главная</title>
+    <link rel="stylesheet" href="./css/pages/index.css">
+</head>
+
+<body>
+    <header class="container">
+        <div class="header__top">
+            <div class="burger-menu">
+                <svg width="27" height="19" viewBox="0 0 27 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill="#212121" fill-rule="evenodd" clip-rule="evenodd"
+                        d="M0.5 1.80435C0.5 1.34206 0.664502 0.889351 0.971298 0.547873C1.27968 0.204632 1.71013 0 2.17188 0H18.5781C19.0399 0 19.4703 0.204633 19.7787 0.547873C20.0855 0.88935 20.25 1.34206 20.25 1.80435C20.25 2.26663 20.0855 2.71935 19.7787 3.06082C19.4703 3.40406 19.0399 3.6087 18.5781 3.6087H2.17188C1.71013 3.6087 1.27968 3.40406 0.971298 3.06082C0.664502 2.71934 0.5 2.26663 0.5 1.80435ZM0.5 17.1957C0.5 16.7334 0.664502 16.2807 0.971298 15.9392C1.27968 15.5959 1.71013 15.3913 2.17188 15.3913H17.0156C17.4774 15.3913 17.9078 15.5959 18.2162 15.9392C18.523 16.2807 18.6875 16.7334 18.6875 17.1957C18.6875 17.6579 18.523 18.1106 18.2162 18.4521C17.9078 18.7954 17.4774 19 17.0156 19H2.17188C1.71013 19 1.27968 18.7954 0.971298 18.4521C0.664503 18.1106 0.5 17.6579 0.5 17.1957ZM0.971298 8.24353C1.27968 7.90029 1.71013 7.69565 2.17188 7.69565H24.8281C25.2899 7.69565 25.7203 7.90029 26.0287 8.24353C26.3355 8.585 26.5 9.03771 26.5 9.5C26.5 9.96229 26.3355 10.415 26.0287 10.7565C25.7203 11.0997 25.2899 11.3043 24.8281 11.3043H2.17188C1.71013 11.3043 1.27968 11.0997 0.971298 10.7565C0.664503 10.415 0.5 9.96229 0.5 9.5C0.5 9.03771 0.664502 8.585 0.971298 8.24353Z" />
+                </svg>
+
+            </div>
+
+            <a class="header__top-link" href="index.php">
+                <svg class="logo-icon" width="27" height="19">
+                    <use href='assets/svg/sprite_header.svg#logo'></use>
+                </svg>
+            </a>
+
+            <button class="search__button">
+                <svg class="logo-icon" width="23" height="13">
+                    <use href='assets/svg/sprite_header.svg#search'></use>
+                </svg>
+            </button>
+
+            <a class="header__top-link" href="catalogue.php">
+                <svg class="logo-icon" width="24" height="24">
+                    <use href='assets/svg/sprite_header.svg#catalogue'></use>
+                </svg>
+            </a>
+
+            <a class="header__top-link" href="profile.php">
+                <svg class="logo-icon" width="24" height="24">
+                    <use href='assets/svg/sprite_header.svg#profile'></use>
+                </svg>
+            </a>
+        </div>
+    </header>
+</body>
+
+</html>
